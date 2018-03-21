93fd9a453dd1868e700b20442b8eb2a6fe37d1a0
Mauro Gonzalez <mauro.d.gonzalez90@gmail.com>
2018-03-21 15:44:08 -0300

    Chnage: Updated project to support example

--- Example/CDNThumbnailKit/Extensions/UIImageViewURL.swift
+++ Example/CDNThumbnailKit/Extensions/UIImageViewURL.swift
@@ -6,4 +6,17 @@
 //  Copyright © 2018 CocoaPods. All rights reserved.
 //
 
+import UIKit
 import Foundation
+import Alamofire
+
+extension UIImageView {
+    public func imageURL(_ url:String){
+        Alamofire.request(url).validate().responseData(completionHandler: { (data) in
+            if let image = UIImage(data: data.data!, scale: UIScreen.main.scale) {
+                self.image = image
+            }
+        })
+
+    }
+}
--- Example/CDNThumbnailKit/ViewController.swift
+++ Example/CDNThumbnailKit/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 import CDNThumbnailKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondImageView: UIImageView!
+    @IBOutlet weak var secondLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.loadImages()
+        
+        
+    }
+
+    @IBAction func reloadImages(_ sender: Any) {
+        self.loadImages()
+    }
+    
+    
+    func loadImages() {
         
         let wideDimension = CDNStrategyConnection()
         wideDimension.add(cdnThumbnail: CDNThumbnail(width: 153, height: 86, type: .center))
@@ -22,33 +39,30 @@ class ViewController: UIViewController {
         wideDimension.add(cdnThumbnail: CDNThumbnail(width: 320, height: 180, type: .center))
         wideDimension.add(cdnThumbnail: CDNThumbnail(width: 375, height: 212, type: .center))
         wideDimension.add(cdnThumbnail: CDNThumbnail(width: 532, height: 298, type: .center))
-        wideDimension.add(cdnThumbnail: CDNThumbnail(width: 650,
-                                                     height: 365, type: .center))
+        wideDimension.add(cdnThumbnail: CDNThumbnail(width: 650, height: 365, type: .center))
         wideDimension.add(cdnThumbnail: CDNThumbnail(width: 760, height: 426, type: .center))
         wideDimension.add(cdnThumbnail: CDNThumbnail(width: 980, height: 550, type: .center))
         wideDimension.add(cdnThumbnail: CDNThumbnail(width: 1024, height: 573, type: .center))
         wideDimension.add(cdnThumbnail: CDNThumbnail(width: 1366, height: 765, type: .center))
-
-        var images = [String]()
-        images.append("http://vodgc.com/p/107/sp/10700/thumbnail/entry_id/0_4ucbsbhf/width/375/height/212/type/2/bgcolor/000000")
-        images.append("http://vodgc.com/p/107/sp/10700/thumbnail/entry_id/0_cp9nzqzy")
-        images.append("https://vignette.wikia.nocookie.net/rockosmodernlife/images/5/5e/Rocko_Wallaby.png")
-
-        var count = 0
-        for image in images{
-            count = count + 1
-            print("ViewController - Image: \(count)")
-            let imageOne = CDNImage(url: URL(string: image)!)
-            
-            print("ViewController - [   Image       ]: \(imageOne.getURL())")
-            print("ViewController - [   Original    ]: \(imageOne.getURL(original: true))")
-            print("ViewController - [   96x96       ]: \(imageOne.getURL(thumbnail: wideDimension.getCDNThumbnail(width: 532, height: 298)))")
-
-        }
         
+        let image = "http://vodgc.com/p/107/sp/10700/thumbnail/entry_id/0_4ucbsbhf/width/375/height/212/type/2/bgcolor/000000"
         
+        let imageOne = CDNImage(url: URL(string: image)!)
+        
+        self.firstImageView.image = nil
+        self.secondImageView.image = nil
+
+        self.firstImageView.imageURL(imageOne.getURL().absoluteString)
+        let thumbnail = wideDimension.getCDNThumbnail(width: 532, height: 298)
+        self.secondImageView.imageURL(imageOne.getURL(thumbnail: thumbnail).absoluteString)
+
+        self.firstLabel.text = "Size original: (375, 212)"
+        self.secondLabel.text = "Size strategy: (\(thumbnail!.width), \(thumbnail!.height) \n \(NetworkAnalyzer.shared.checkType()) )"
+
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
