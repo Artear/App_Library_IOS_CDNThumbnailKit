8bfec5dcae09ff5c171c544797425cac6b384d21
Mauro Gonzalez <mauro.d.gonzalez90@gmail.com>
2018-03-21 10:47:06 -0300

    Fix: CDNStrategyConnection making init public

--- CDNThumbnailKit/Classes/Strategy/CDNStrategyConnection.swift
+++ CDNThumbnailKit/Classes/Strategy/CDNStrategyConnection.swift
@@ -11,7 +11,7 @@ public class CDNStrategyConnection: CDNStrategyProtocol {
     
     var strategyDefault : CDNStrategyProtocol!
     
-    init() {
+    public init() {
         self.strategyDefault = CDNStrategyDefault()
     }
     
