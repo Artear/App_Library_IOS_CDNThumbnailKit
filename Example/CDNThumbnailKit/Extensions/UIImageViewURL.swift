7bec29255cee17949789c322ecd39f46a98fae07
Mauro Gonzalez <mauro.d.gonzalez90@gmail.com>
2018-03-21 15:41:49 -0300

    Add: UIImageVIewURL extension

---
+++ Example/CDNThumbnailKit/Extensions/UIImageViewURL.swift
@@ -0,0 +1,9 @@
+//
+//  UIImageViewURL.swift
+//  CDNThumbnailKit_Example
+//
+//  Created by Mauro Gonzalez on 21/03/2018.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import Foundation
